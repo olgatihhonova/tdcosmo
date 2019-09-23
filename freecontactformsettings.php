dc96451e4a212c2f6a330b1db7463251bd0aa007
olgatihhonova <olgatihhonova@users.noreply.github.com>
2019-09-23 18:55:26 +0300

    first commit

---
+++ freecontactformsettings.php
@@ -0,0 +1,12 @@
+<?php
+$email_to = "youremailaddress@gmail.com"; // your email address
+$email_subject = "tdcosmo webpage"; // email subject line
+$thankyou = "thankyou.htm"; // thank you page
+
+// if you update the question on the form -
+// you need to update the questions answer below
+$antispam_answer = "25";
+
+// Do not change this line
+$base = "CgpGb3JtIHBvd2VyZWQgYnkgaHR0cHM6Ly93d3cuZnJlZWNvbnRhY3Rmb3JtLmNvbQ==";
+?>
